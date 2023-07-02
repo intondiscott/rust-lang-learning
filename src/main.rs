201bbfe487389878312c569600fd68a51628f8dc
intondiscott@gmail.com <you@example.com>
2023-07-02 09:26:25 -0400

    file updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+struct User {
+    name: String,
+    age: u8,
+    email: String,
 }
+impl User {
+    fn greeting(&self) {
+    println!("Welcome {}!",&self.name);
+    }
+    fn user_info(&self){
+        println!("{{\n\tname: {}, \n\tage: {}, \n\temail: {}\n}}", &self.name, &self.age, &self.email);
+    }
+}
+
+fn main() {
+    let user1 = User{name: "Scotty".to_string(),age: 37, email: "intondiscott@gmail.com".to_string()};
+    user1.greeting();
+    user1.user_info();
+}
+
+
